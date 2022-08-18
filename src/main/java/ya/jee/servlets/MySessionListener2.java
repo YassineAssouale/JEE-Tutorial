8b279e91fae0f2d220e414d97073e7c0d2a2c487
Yassine <yassine.assouale@gamil.com>
2022-08-19 00:24:46 +0200

    Creation a second listener for session by implenting HttpSessionAttributeListener

---
+++ src/main/java/ya/jee/servlets/MySessionListener2.java
@@ -0,0 +1,32 @@
+package ya.jee.servlets;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+/**
+ * Class qui permet d'écouter les attributs de session
+ * @author yassi
+ *
+ */
+@WebListener
+public class MySessionListener2 implements HttpSessionAttributeListener{
+ 
+	 @Override
+	public void attributeAdded(HttpSessionBindingEvent event) {
+		// TODO Auto-generated method stub
+		HttpSessionAttributeListener.super.attributeAdded(event);
+	}
+	 
+	 @Override
+	public void attributeReplaced(HttpSessionBindingEvent event) {
+		// TODO Auto-generated method stub
+		HttpSessionAttributeListener.super.attributeReplaced(event);
+	}
+	 
+	 @Override
+	public void attributeRemoved(HttpSessionBindingEvent event) {
+		// TODO Auto-generated method stub
+		HttpSessionAttributeListener.super.attributeRemoved(event);
+	}
+}
